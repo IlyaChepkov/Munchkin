a16a533aa7b52358b548f243240822ebf5f2f359
IlyaChepkov <ilya.chepkov2011@gmail.com>
2023-07-10 12:05:58 +0300

    Player
    ???

---
+++ Munchkin/Munchkin/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace Munchkin
+{
+    public class Player
+    {
+        readonly string playerName;
+        readonly bool isMale;
+        byte level = 1;
+        Munchkin munchkin;
+    }
+}
