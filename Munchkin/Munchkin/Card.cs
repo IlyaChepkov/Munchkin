c72026e49093ad77b746c1d68c2f3486c6d81034
IlyaChepkov <ilya.chepkov2011@gmail.com>
2023-08-04 14:43:26 +0300

    создание карт

--- Munchkin/Munchkin/Card.cs
+++ Munchkin/Munchkin/Card.cs
@@ -1,17 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.SymbolStore;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Xml;
 
 namespace Munchkin
 {
-    public class Card
+    public abstract class Card
     {
         readonly int? price;
         readonly string name;
         internal readonly bool isDoor;
         readonly string image;
+
+        public static List<Card> CreateCards(XmlNode node)
+        {
+            if (node.Name == "Cards")
+            {
+                List<Card> cards = new List<Card>();
+                foreach (XmlNode card in node.ChildNodes)
+                {
+                    if (card.Name == "Card")
+                    {
+                        int count = 1;
+                        XmlNode countNode = GetFirstXmlNode(card.ChildNodes, "Count");
+                        if (countNode != null)
+                        {
+                            count = int.Parse(countNode.InnerText);
+                        }
+                        XmlNode imagesNode = GetFirstXmlNode(card.ChildNodes, "Images");
+                        if (imagesNode != null && countNode == null)
+                        {
+                            count = imagesNode.ChildNodes.Count;
+                        }
+                        string cardType = GetFirstXmlNode(card.ChildNodes, "Type").InnerText;
+                        bool isDoor = bool.Parse(GetFirstXmlNode(card.ChildNodes, "IsDoor").InnerText);
+                        string name = GetFirstXmlNode(card.ChildNodes, "Name").InnerText;
+                        int? price = null;
+                        if (GetFirstXmlNode(card.ChildNodes, "Price") != null)
+                            price = int.Parse(GetFirstXmlNode(card.ChildNodes, "Price").InnerText);
+                        for (int i = 0; i < count; i++)
+                        {
+
+                        }
+                    }
+                }
+                return cards;
+            }
+            else
+                return null;
+        }
+
+        private static XmlNode GetFirstXmlNode(XmlNodeList list, string name)
+        {
+            foreach (XmlNode node in list)
+            {
+                if (node.Name == name)
+                    return node;
+            }
+            return null;
+        }
     }
 }
